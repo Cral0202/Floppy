b981d0664da2b80748300d480a59feafae8f5bee
Kews02 <3tigercarl@gmail.com>
2023-08-20 21:00:37 +0200

    Escape now exits game

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    // The window
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Floppy", sf::Style::Default);
+    
+    // A green circle
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    // The game loop
     while (window.isOpen()) {
+        // Event polling
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            // Closes the window
+            if (event.type == sf::Event::Closed) {
                 window.close();
+                break;
+            }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                    break;
+                }
+            }
+                
         }
 
-        window.clear();
+        // Update game state
+
+
+        // Render game
+        window.clear(); // Clear old frame
+
         window.draw(shape);
-        window.display();
+
+        window.display(); // Draws what has been rendered so far
     }
 
     return 0;
